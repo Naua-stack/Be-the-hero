b7b42520ca756017eb5499c51dd86ab09444e073
xnauax <62560321+xnauax@users.noreply.github.com>
2020-03-24 23:00:33 -0300

    Back-end da Aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Minha namorada Larissa SC é a mais linda do mundo e ganhou o Miss Brasil
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
